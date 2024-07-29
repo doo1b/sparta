2622ef356716da820c4b9121c729cfbb2a720b23
doo1b <tngml928@gmail.com>
2024-07-29 14:59:08 +0900

    final

--- script.js
+++ script.js
@@ -118,8 +118,7 @@ window.onclick = (e) => {
   if (!e.target.matches('.dropbtn_click')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
 
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
+    for (var i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
       if (openDropdown.classList.contains('show')) {
         openDropdown.classList.remove('show');
